23e554e5da3eec7fb88dddd76fee67f1a9d58b01
Szh022401 <125086228+Szh022401@users.noreply.github.com>
2024-03-02 01:55:17 -0500

    Update 179. All Valid Permutations Of Parentheses II.java

--- 179. All Valid Permutations Of Parentheses II.java
+++ 179. All Valid Permutations Of Parentheses II.java
@@ -1,3 +1,15 @@
+// Hard
+// Get all valid permutations of l pairs of (), 
+// m pairs of <> and n pairs of {}.
+
+// Assumptions
+
+// l, m, n >= 0
+// l + m + n > 0
+// Examples
+
+// l = 1, m = 1, n = 0, all the valid permutations are ["()<>", "(<>)", "<()>", "<>()"]
+
 import java.util.List;
 import java.util.Deque;
 import java.util.LinkedList;
