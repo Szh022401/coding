91d42b772f6227744d84a11194eea6c4f747c5c2
Szh022401 <125086228+Szh022401@users.noreply.github.com>
2024-03-02 01:54:44 -0500

    Create 179. All Valid Permutations Of Parentheses II.java

---
+++ 179. All Valid Permutations Of Parentheses II.java
@@ -0,0 +1,62 @@
+import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Solution {
+  private static final char[] brackets = new char[]{'(', ')', '<', '>', '{', '}'};
+
+  public List<String> validParentheses(int l, int m, int n) {
+    
+    int[] remain = new int[]{l, l, m, m, n, n};
+    Deque<Character> stack = new LinkedList<>();
+    List<String> ans = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    int totalLength = l * 2 + m * 2 + n * 2;
+
+    helper(brackets, remain, stack, ans, sb, totalLength);
+
+    return ans;
+  }
+
+  private void helper(char[] brackets, int[] remain, Deque<Character> stack, List<String> ans, StringBuilder sb, int totalLength) {
+    if (sb.length() == totalLength) {
+      ans.add(sb.toString());
+      return;
+    }
+
+    for (int i = 0; i < remain.length; ++i) {
+      if (i % 2 == 0) {
+        if (remain[i] > 0) {
+          sb.append(brackets[i]);
+          stack.offerFirst(brackets[i]);
+          --remain[i];
+          helper(brackets, remain, stack, ans, sb, totalLength);
+          sb.deleteCharAt(sb.length() - 1);
+          stack.pollFirst();
+          ++remain[i];
+        }
+      }
+      else {
+        if (!stack.isEmpty() && (stack.peekFirst() == brackets[i - 1])) {
+          sb.append(brackets[i]);
+          stack.pollFirst();
+          --remain[i];
+          helper(brackets, remain, stack, ans, sb, totalLength);
+          sb.deleteCharAt(sb.length() - 1);
+          stack.offerFirst(brackets[i - 1]);
+          ++remain[i];
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    List<String> result = solution.validParentheses(1, 1, 1);
+    System.out.println("Total combinations: " + result.size());
+    for (String s : result) {
+      System.out.println(s);
+    }
+  }
+}
